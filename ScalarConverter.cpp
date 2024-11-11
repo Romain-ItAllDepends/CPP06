6bc28091e207cb73f8d47aa9dfc35021c847af82
Romain Gobet <rgobet@2G5.42angouleme.fr>
2024-11-11 16:01:49 +0100

    ex00 need to do range and done

--- ScalarConverter.cpp
+++ ScalarConverter.cpp
@@ -6,11 +6,12 @@
 /*   By: rgobet <rgobet@student.42angouleme.fr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/09 14:25:19 by rgobet            #+#    #+#             */
-/*   Updated: 2024/11/09 16:02:06 by rgobet           ###   ########.fr       */
+/*   Updated: 2024/11/11 15:58:11 by rgobet           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ScalarConverter.hpp"
+#include <cstdlib>
 
 ScalarConverter::ScalarConverter(void) {
 	std::cout << "ScalarConverter init." << std::endl;
@@ -20,45 +21,149 @@ ScalarConverter::~ScalarConverter(void) {
 	std::cout << "ScalarConverter destroy." << std::endl;
 }
 
+ScalarConverter::ScalarConverter(ScalarConverter const &obj) {
+	*this = obj;
+}
+
+ScalarConverter const & ScalarConverter::operator=(ScalarConverter const &a) {
+	(void)a;
+    return *this;
+}
+
+static bool	isDigit(const char *s) {
+	int	i = 0;
+	int	dot = 0;
+	
+	while (s[i]) {
+		if (s[i] == '.')
+			dot++;
+		if ((!(s[i] > 47 && 58 > s[i]) && s[i] != '.') || dot > 1)
+			return (false);
+		if (!s[i + 1] && s[i] == 'f')
+			break;
+		i++;
+	}
+	return (true);
+}
+
+static char	tmp(char *s) {
+	int	i = 0;
+	int	tmp = 0;
+	int	sign = 1;
+
+	if (s[0] == '-')
+		sign *= 1;
+	while (s[i] && s[i] != '.') {
+		tmp *= 10;
+		tmp += s[i] - 48;
+		i++;
+	}
+	return (tmp * sign);
+}
+
 static char	convertChar(std::string c) {
-	return (c[0]);
+	char	x[c.length() + 1];
+
+	strcpy(x, c.c_str());
+	return (tmp(x));
 }
 
 static double	convertDouble(std::string d) {
-	char	number[d.length() + 1];
+	char	x[d.length() + 1];
 
-	strcpy(number, d.c_str());
-	return (atof(number));
+	strcpy(x, d.c_str());
+	return (static_cast<double>(strtold(x, NULL)));
 }
 
 static float	convertFloat(std::string f) {
-	return (std::stof(f));
+	char	x[f.length() + 1];
+
+	strcpy(x, f.c_str());
+	return (static_cast<float>(strtold(x, NULL)));
 }
 
 static long	convertLong(std::string l) {
-	char	number[l.length() + 1];
+	char	x[l.length() + 1];
 
-	strcpy(number, l.c_str());
-	return (atof(number));
+	strcpy(x, l.c_str());
+	return (static_cast<long>(strtoll(x, NULL, 10)));
 }
 
-static bool	isPrintable(char c) {
+static bool	isPrintable(std::string str) {
+	char	x[str.length() + 1];
+	int	c;
+	
+	strcpy(x, str.c_str());
+	c = atoi(x);
+	if (str.length() > 3 || isDigit(x) == false)
+		return (false);
+	return (true);
+}
+
+static bool	isDisplayable(std::string str) {
+	char	x[str.length() + 1];
+	int	c;
+	
+	strcpy(x, str.c_str());
+	c = atoi(x);
 	if (!(c < 127 && c > 31))
 		return (false);
 	return (true);
 }
 
-static void	*convert(std::string n) {
+void	ScalarConverter::convert(std::string n) {
 	const long	verif = convertLong(n);
-	if (isPrintable(convertChar(n)) == false)
+	char	x[n.length() + 1];
+	
+	strcpy(x, n.c_str());
+	
+	/* EXCEPTION */ /* Maybe useless */
+
+	if (n == "nan" || n == "nanf" || n == "-inf" || n == "+inf"
+		|| n == "-inff" || n == "+inff") {
+		std::cout << "char: impossible" << std::endl;
+		std::cout << "int: impossible" << std::endl;
+		if (n == "nan" || n == "nanf")
+			std::cout << "float: nanf" << std::endl;
+		else
+			std::cout << "float: " << n << std::endl;
+		std::cout << "double: " << n << std::endl;
+		return ;
+	}
+
+	/* CHAR */ /* DONE (but need to verify the convertion float to char) */
+
+	if (isDisplayable(n) == false)
 		std::cout << "char: Non displayable" << std::endl;
-	else if (verif < 0 || verif > 128)
+	else if (verif < 0 || verif > 128 || isPrintable(n))
 		std::cout << "char: impossible" << std::endl;
 	else
-		std::cout << "char: " << convertChar(n) << std::endl;
-	if (verif < 2147483648 && -2147483649 < verif)
+		std::cout << "char: '" << convertChar(n) << "'" << std::endl;
+
+	/* INT */ /* DONE */
+	std::cout << isDigit(x) << std::endl;
+	if (verif > 2147483647 || -2147483648 > verif || isDigit(x))
 		std::cout << "int: impossible" << std::endl;
 	else
 		std::cout << "int: " << verif << std::endl;
-	if (__FLT_MIN__ )
+
+	/* FLOAT */
+
+	if (verif - convertFloat(n) == 0)
+		std::cout << "float: " << convertFloat(n) << ".0f" << std::endl;
+	else
+		std::cout << "float: " << convertFloat(n) << "f" << std::endl;
+	// if (__FLT_MIN__ ) // Verif la range en entier
+	// else if (n == "nan")
+	// 	std::cout << "float: nanf" << std::endl;
+
+	// /* DOUBLE */
+
+	if (verif - convertFloat(n) == 0)
+		std::cout << "double: " << convertDouble(n) << ".0" << std::endl;
+	else
+		std::cout << "double: " << convertDouble(n) << std::endl;
+	// if (n == "nan") // Verif la range en entier
+	// 	std::cout << "double: nan" << std::endl;
+
 }
--- main.cpp
+++ main.cpp
@@ -1,28 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ScalarConverter.hpp                                :+:      :+:    :+:   */
+/*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: rgobet <rgobet@student.42angouleme.fr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/11/09 14:25:14 by rgobet            #+#    #+#             */
-/*   Updated: 2024/11/11 15:47:48 by rgobet           ###   ########.fr       */
+/*   Created: 2024/11/11 12:23:29 by rgobet            #+#    #+#             */
+/*   Updated: 2024/11/11 14:55:05 by rgobet           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef SCALARCONVERTER_HPP
-# define SCALARCONVERTER_HPP
-# include <iostream>
-# include <string.h>
+#include "ScalarConverter.hpp"
 
-class ScalarConverter {
-	public:
-		ScalarConverter(void);
-		ScalarConverter(ScalarConverter const &obj);
-		~ScalarConverter(void);
-		ScalarConverter const &operator=(ScalarConverter const &a);
-
-		static void	convert(std::string n);
-};
-
-#endif
+int	main(int ac, char *av[]) {
+	if (ac != 2) {
+		std::cerr << "Error: only one argument is required." << std::endl;
+		return (1);
+	}
+	ScalarConverter::convert(av[1]);
+	return (0);
+}
