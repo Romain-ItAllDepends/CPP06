5ffee741a2b771f9c092086ec4f6f61d2caefdc7
Romain Gobet <rgobet@2G4.42angouleme.fr>
2024-11-15 15:57:37 +0100

    Finish ex01

--- ex01/Serializer.cpp
+++ ex01/Serializer.cpp
@@ -6,7 +6,7 @@
 /*   By: rgobet <rgobet@student.42angouleme.fr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/15 14:24:23 by rgobet            #+#    #+#             */
-/*   Updated: 2024/11/15 14:59:58 by rgobet           ###   ########.fr       */
+/*   Updated: 2024/11/15 15:53:43 by rgobet           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,11 @@
 Serializer::Serializer(void) {
 }
 
-Serializer::~Serializer(void) {
-}
-
 Serializer::Serializer(Serializer const &obj) {
 	*this = obj;
+}
+
+Serializer::~Serializer(void) {
 }
 
 Serializer const &Serializer::operator=(Serializer const &obj) {
@@ -27,10 +27,10 @@ Serializer const &Serializer::operator=(Serializer const &obj) {
 	return (*this);
 }
 
-uintptr_t serialize(Data* ptr) {
+uintptr_t Serializer::serialize(Data* ptr) {
 	return (reinterpret_cast<uintptr_t>(ptr));
 }
 
-Data* deserialize(uintptr_t raw) {
+Data* Serializer::deserialize(uintptr_t raw) {
 	return (reinterpret_cast<Data *>(raw));
 }
--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: rgobet <rgobet@student.42angouleme.fr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/15 15:00:14 by rgobet            #+#    #+#             */
-/*   Updated: 2024/11/15 15:18:44 by rgobet           ###   ########.fr       */
+/*   Updated: 2024/11/15 15:56:07 by rgobet           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,6 @@ int	main(void) {
 	test.content = 8;
 	verif = Serializer::serialize(&test);
 	std::cout << "Serialize (Data -> uintptr_t): " << verif << std::endl;
-	std::cout << "Deserialize (uintptr_t -> Data): " << Serializer::deserialize(verif) << std::endl;
+	std::cout << "Deserialize (uintptr_t -> Data): " << Serializer::deserialize(verif)->content << std::endl;
 	return (0);
 }
